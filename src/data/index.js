3b13385b40dd202de6b334e87503c5bfcce8b2f7
IlhamTahir <yantaq@bilig.biz>
2018-08-10 14:21:32 +0800

    fix

--- src/data/index.js
+++ src/data/index.js
@@ -9,13 +9,7 @@ const sideMenu = [
     title: '课程',
     icon: 'cvp-icon cvp-icon-course',
     isShowSub: false,
-    children: [
-      {
-        title: '所有课程',
-        // icon: 'cvp-icon cvp-icon-home',
-        routeName: 'courseList'
-      }
-    ]
+    routeName: 'courseList'
   }
 ]
 
